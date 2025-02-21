65b4647a0dcbca91a9b3ad87ba59e178bd0de529
neojarvis <techadmin@iamneo.ai>
2025-02-21 14:35:40 +0000

    Auto-save-commit-2/21/2025--8:05:39-PM

--- springapp/src/main/java/com/examly/springapp/model/User.java
+++ springapp/src/main/java/com/examly/springapp/model/User.java
@@ -1,13 +1,22 @@
 package com.examly.springapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String password;
@@ -21,75 +30,6 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Like> likes;
 
-    // Constructors, Getters, and Setters
-    public User() {
-    }
-
-    public User(Long id, String name, String email, String password, List<Post> posts, List<Comment> comments,
-            List<Like> likes) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.posts = posts;
-        this.comments = comments;
-        this.likes = likes;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<Post> getPosts() {
-        return posts;
-    }
-
-    public void setPosts(List<Post> posts) {
-        this.posts = posts;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
-
-    public List<Like> getLikes() {
-        return likes;
-    }
-
-    public void setLikes(List<Like> likes) {
-        this.likes = likes;
-    }
+    @Column(name = "profile_pic_url")
+    private String profilePicUrl;
 }
